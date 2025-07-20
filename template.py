25fee4d0f3bf03a176b84fde9532a51f7c0753ee
nishan0000 <mdnishan006@gmail.com>
2025-07-20 12:06:25 +0530

    Custom logging implemented

--- template.py
+++ template.py
@@ -9,26 +9,28 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 project_name = 'datascience'
 
 list_of_files=[
-    ".github/workflows/.gitkeep", # Deployment
-    f"src/{project_name}/__init__.py", # Packaging
-    f"src/{project_name}/components.py", # Pipelines
-    f"src/{project_name}/utils.py", # Generic fncts
-    f"src/{project_name}/utils/common.py", # Common functionalities
+    ".gthub/workflows/.gitkeep",
+    # f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py", 
+    f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
-    "main.py",
-    "setup.py",
+    # "main.py",
     "Dockerfile",
+    "setup.py",
     "research/research.ipynb",
-    "templates/index.html"
-    ]
+    "templates/index.html",
+    "app.py"
+
+]
 
 for file_path in list_of_files:
     file_path = Path(file_path)
